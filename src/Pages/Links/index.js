8266b053f8e1f51b8833da17372174b499310bfc
Wade Beall <wadegb12@okstate.edu>
2018-09-04 21:54:49 -0500

    updated links page

--- src/Pages/Links/index.js
+++ src/Pages/Links/index.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react'
-import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
-
-import { Page, ComingSoon } from '../../Components'
+import { Page, Body } from '../../Components'
 
 const Links = (props) => {
     return ( 
         <Page history={props.history}>
-            <ComingSoon />
+            <Body>
+                <div style={{ textAlign:"center", padding:"10px"}}>
+                    <Typography> 
+                        <div style={{padding:"10px"}}>
+                            <a href="http://www.oksoccer.com/referee/refereehome/"> OSA Referee Page </a>
+                        </div>
+                        <div style={{padding:"10px"}}>
+                            <a href="http://www.okpremierclubs.com/home.php?layout=4604125"> OPC Referee Page </a>
+                        </div>
+                        <div style={{padding:"10px"}}>
+                            <a href="http://www.oksoccer.com/classes/"> Referee Classes </a>
+                        </div>
+                        <div style={{padding:"10px"}}>
+                            <a href="https://idevforms.americaneagle.com/oklahomasa/form509128109/secure_index.html"> Misconduct Form </a>
+                        </div>
+                    </Typography> 
+                </div>
+            </Body>
         </Page>
     )
 }
